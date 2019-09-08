8c09b2190900f76bfe9e6f2e26322c4c7b44d861
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2019-09-08 08:19:30 +0800

    Initial Commit

---
+++ DeleteCell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeleteCell
+//
+//  Created by Runhua Huang on 2019/9/8.
+//  Copyright © 2019 Runhua Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
